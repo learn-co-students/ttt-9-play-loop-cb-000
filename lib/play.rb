64994ead7de0f1bd54e97eb12fc3342677115548
David Hopkins <dlhopkins2@gmail.com>
2017-09-06 01:14:01 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turns = 0
-  while turns < 9
+  turn_count = 0
+  while turn_count < 9
     turn(board)
-    turns = turns + 1
+    turn_count+=1
   end
 end
