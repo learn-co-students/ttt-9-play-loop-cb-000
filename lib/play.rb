912e3fdf7efa822cc12959e476b4e0faefdce814
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-07 14:40:59 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+=begin
+  9.times{
+    turn(board)
+  }
+=end
+turn_number = 0
+while turn_number < 9
+  turn(board)
+  turn_number +=1
+end
+end
