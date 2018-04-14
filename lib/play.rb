95e8b41043a360fc94a4ab96a926c8333eac73f5
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-14 02:59:41 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -37,5 +37,12 @@ end
 
 # Define your play method below
 def play(board)
-  turn()
-end
+  counter = 0
+  loop do
+      counter += 1
+      turn(board)
+        if counter >= 9
+          break
+        end
+    end
+  end
